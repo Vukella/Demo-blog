c8dfba3b3b2b638ca8c5ec64ee3251807f829d40
Vukella <nemanja.vukelic@outlook.com>
2023-11-28 12:46:02 +0100

    adds layouts folder (inside views folder) alongside with footer, head and header blade.php files (in every of those files is pasted section from index.blade.php file)

---
+++ appside-app/resources/views/layouts/head.blade.php
@@ -0,0 +1,26 @@
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="robots" content="noarchive">
+    <title> Appside | App Landing pages HTML Templates </title>
+    <!-- favicon -->
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <!-- bootstrap -->
+    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css')}}">
+    <!-- icofont -->
+    <link rel="stylesheet" href="{{ asset('assets/css/fontawesome.5.7.2.css')}}">
+    <!-- flaticon -->
+    <link rel="stylesheet" href="{{ asset('assets/css/flaticon.css')}}">
+    <!-- animate.css -->
+    <link rel="stylesheet" href="{{ asset('assets/css/animate.css')}}">
+    <!-- Owl Carousel -->
+    <link rel="stylesheet" href="{{ asset('assets/css/owl.carousel.min.css')}}">
+    <!-- magnific popup -->
+    <link rel="stylesheet" href="{{ asset('assets/css/magnific-popup.css')}}">
+    <!-- stylesheet -->
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css')}}">
+    <!-- responsive -->
+    <link rel="stylesheet" href="{{ asset('assets/css/responsive.css')}}">
+</head>
