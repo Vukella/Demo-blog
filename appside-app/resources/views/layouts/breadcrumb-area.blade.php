c701351ce215957b5bd086bdb84d30b32dd04f5e
Vukella <nemanja.vukelic@outlook.com>
2023-11-28 13:24:34 +0100

    adds in layout folder breadcrumb-area, navbar, preloader-area and scripts as blade.php files

---
+++ appside-app/resources/views/layouts/breadcrumb-area.blade.php
@@ -0,0 +1,15 @@
+<div class="breadcrumb-area breadcrumb-bg extra">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="breadcrumb-inner">
+                    <h1 class="page-title">Blog Details</h1>
+                    <ul class="page-navigation">
+                        <li><a href="#"> Home</a></li>
+                        <li>Blog Details</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ appside-app/resources/views/layouts/scripts.blade.php
@@ -0,0 +1,20 @@
+    <!-- jquery -->
+    <script src="{{ asset('assets/js/jquery.js') }}"></script>
+    <!-- popper -->
+    <script src="{{ asset('assets/js/popper.min.js') }}"></script>
+    <!-- bootstrap -->
+    <script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
+    <!-- owl carousel -->
+    <script src="{{ asset('assets/js/owl.carousel.min.js') }}"></script>
+    <!-- magnific popup -->
+    <script src="{{ asset('assets/js/jquery.magnific-popup.js') }}"></script>
+    <!-- contact js-->
+    <script src="{{ asset('assets/js/contact.js') }}"></script>
+    <!-- wow js-->
+    <script src="{{ asset('assets/js/wow.min.js') }}"></script>
+    <!-- way points js-->
+    <script src="{{ asset('assets/js/waypoints.min.js') }}"></script>
+    <!-- counterup js-->
+    <script src="{{ asset('assets/js/jquery.counterup.min.js') }}"></script>
+    <!-- main -->
+    <script src="{{ asset('assets/js/main.js') }}"></script>
