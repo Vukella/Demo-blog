008bdc7d6c8189bae1be11a15fda16372f9e6275
Vukella <nemanja.vukelic@outlook.com>
2023-12-01 17:56:06 +0100

    adds all nececary attributes for all tables

--- appside-app/database/migrations/2023_11_28_220301_create_blogs_table.php
+++ appside-app/database/migrations/2023_11_28_220301_create_blogs_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('blogs', function (Blueprint $table) {
             $table->id();
             $table->string('title');
+            $table->foreignId('category_id');
+            $table->foreignId('tag_id');
             $table->text('story')->nullable();
             $table->date('published')->nullable();
             $table->timestamps();
