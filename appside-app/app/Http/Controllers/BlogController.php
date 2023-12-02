f1315adb8b3096b1a2f9efebea0a2ec359f93493
Vukella <nemanja.vukelic@outlook.com>
2023-12-02 11:28:55 +0100

    changes Blog Controller for needs of public site

--- appside-app/app/Http/Controllers/BlogController.php
+++ appside-app/app/Http/Controllers/BlogController.php
@@ -2,9 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Blog;
 use Illuminate\Http\Request;
 
 class BlogController extends Controller
 {
-    //
+    public function index()
+    {
+        $blogs = Blog::all(); 
+        return view('blog.index', compact('blogs'));
+    }
+
+    public function show(Blog $blog)
+    {
+        return view('blog.show', compact('blog'));
+    }
 }
+
+
+
+
+?>
