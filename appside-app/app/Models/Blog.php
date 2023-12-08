4a3d24c7c9abc3989b2e97db2370631fdf9cc39a
Vukella <nemanja.vukelic@outlook.com>
2023-12-08 23:08:51 +0100

    updates models

--- appside-app/app/Models/Blog.php
+++ appside-app/app/Models/Blog.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Blog extends Model
 {
     use HasFactory;
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class);
+    }
+
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class);
+    }
 }
