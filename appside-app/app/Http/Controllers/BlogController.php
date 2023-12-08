0d0a42e961165714b098170d82eddcbbf0f264b0
Vukella <nemanja.vukelic@outlook.com>
2023-12-09 00:15:07 +0100

    adds routes to web.php and adds index and show functions inside BlogController

--- appside-app/app/Http/Controllers/BlogController.php
+++ appside-app/app/Http/Controllers/BlogController.php
@@ -2,9 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Blog;
 use Illuminate\Http\Request;
 
 class BlogController extends Controller
 {
     //
+    public function index()
+    {
+        return view('blogs.index',[
+            'blogs' => Blog::latest()->filter(
+                request(['category','authors'])
+                )->paginate(10)->withQueryString()
+        ]);
+    }
+
+    public function show(Blog $blog)
+    {
+        return view('blogs.show',[
+            'blog'=> $blog
+            ]);
+    }
 }
--- appside-app/routes/web.php
+++ appside-app/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BlogController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,13 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::get('/', function () {return view('index');})->name('home');
 
-Route::get('/blog', function () {
-    return view('blog');
-});
+Route::get('/blogs', [BlogController::class, 'index'])->name('blogs');
+Route::get('blogss/{blog:slug}', [BlogController::class, 'show']);
 
 Route::get('/blog-details', function () {
     return view('blog-details');
